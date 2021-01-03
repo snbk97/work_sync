93f836ce62a9695e0f7d68daa4357e3fc89704e5
Sayan Bhowmik <snbk97@gmail.com>
2021-01-03 18:37:22 +0530

    serverless sync

---
+++ serverless-udemy/auction-service/src/handlers/createAuction.js
@@ -0,0 +1,27 @@
+import {v4 as uuid} from 'uuid';
+import AWS from 'aws-sdk';
+
+const Dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+  const { title } = event.body;
+  const now = new Date().toISOString();
+  const auction = {
+    id: uuid(),
+    title, 
+    status: 'OPEN',
+    createdAt: now,
+  }
+  Dynamodb.put({
+    TableName:  'AuctionTable',
+    Item: auction,
+  }).promise();
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ auction }),
+  };
+}
+
+export const handler = createAuction;
+
+
